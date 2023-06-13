e84517502ca1b99557e6fd8f5a01b143b048bdcd
Hossam Haggag <haggagdev@gmail.com>
2023-06-14 01:44:33 +0300

    update search location

--- lib/mainScreens/search_places_screen.dart
+++ lib/mainScreens/search_places_screen.dart
@@ -16,8 +16,8 @@ class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
     if (inputText.length > 1) //2 or more than 2 input characters
     {
       String urlAutoCompleteSearch =
-          "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$inputText&key=$mapKey&components=country:EG";
-
+          "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$inputText&key=$mapKey&components=country:SA";
+ 
       var responseAutoCompleteSearch =
           await RequestAssistant.receiveRequest(urlAutoCompleteSearch);
 
